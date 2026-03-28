a0d34c94e15c7f92637cbfd40530bf4963a6c5a2
juan.vasquez <juan.vasquez@pleo.io>
2026-03-28 15:27:51 +0000

    init pwa template

---
+++ vite.config.ts
@@ -0,0 +1,39 @@
+import { VitePWA } from "vite-plugin-pwa";
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [
+    react(),
+    VitePWA({
+      registerType: "autoUpdate",
+      injectRegister: false,
+
+      pwaAssets: {
+        disabled: false,
+        config: true,
+      },
+
+      manifest: {
+        name: "guide-me-app",
+        short_name: "guide-me-app",
+        description: "guide-me-app",
+        theme_color: "#ffffff",
+      },
+
+      workbox: {
+        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+        cleanupOutdatedCaches: true,
+        clientsClaim: true,
+      },
+
+      devOptions: {
+        enabled: true,
+        navigateFallback: "index.html",
+        suppressWarnings: true,
+        type: "module",
+      },
+    }),
+  ],
+});
