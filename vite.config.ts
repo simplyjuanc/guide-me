011396edda43f233f6d9fbdcff2457455d27e2aa
juan.vasquez <juan.vasquez@pleo.io>
2026-03-29 17:54:32 +0100

    fix typo

--- vite.config.ts
+++ vite.config.ts
@@ -4,66 +4,70 @@ import react from "@vitejs/plugin-react";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: "/guide-me-app/",
-  plugins: [
-    react(),
-    VitePWA({
-      registerType: "autoUpdate",
-      injectRegister: false,
+	base: "/guide-me/",
+	plugins: [
+		react(),
+		VitePWA({
+			registerType: "autoUpdate",
+			injectRegister: false,
 
-      pwaAssets: {
-        disabled: false,
-        config: true,
-      },
+			pwaAssets: {
+				disabled: false,
+				config: true,
+			},
 
-      includeAssets: ["favicon.ico", "apple-touch-icon-180x180.png", "mask-icon.svg"],
-      manifest: {
-        name: "guide-me-app",
-        short_name: "guide-me-app",
-        display: "standalone",
-        start_url: "/",
-        description: "guide-me-app",
-        theme_color: "#ffffff",
-        icons: [
-          {
-            src: "pwa-192x192.png",
-            sizes: "192x192",
-            type: "image/png",
-          },
-          {
-            src: "pwa-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "any",
-          },
-          {
-            src: "maskable-icon-512x512.png",
-            sizes: "512x512",
-            type: "image/png",
-            purpose: "maskable",
-          },
-          {
-            src: "pwa-64x64.png",
-            sizes: "64x64",
-            type: "image/png",
-            purpose: "any",
-            // purpose: "maskable any",
-          },
-        ],
-      },
+			includeAssets: [
+				"favicon.ico",
+				"apple-touch-icon-180x180.png",
+				"mask-icon.svg",
+			],
+			manifest: {
+				name: "guide-me-app",
+				short_name: "guide-me-app",
+				display: "standalone",
+				start_url: "/guide-me/",
+				description: "guide-me-app",
+				theme_color: "#ffffff",
+				icons: [
+					{
+						src: "pwa-192x192.png",
+						sizes: "192x192",
+						type: "image/png",
+					},
+					{
+						src: "pwa-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "any",
+					},
+					{
+						src: "maskable-icon-512x512.png",
+						sizes: "512x512",
+						type: "image/png",
+						purpose: "maskable",
+					},
+					{
+						src: "pwa-64x64.png",
+						sizes: "64x64",
+						type: "image/png",
+						purpose: "any",
+						// purpose: "maskable any",
+					},
+				],
+			},
 
-      workbox: {
-        globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
-        cleanupOutdatedCaches: true,
-        clientsClaim: true,
-      },
+			workbox: {
+				globPatterns: ["**/*.{js,css,html,svg,png,ico}"],
+				cleanupOutdatedCaches: true,
+				clientsClaim: true,
+			},
 
-      devOptions: {
-        enabled: true,
-        navigateFallback: "index.html",
-        suppressWarnings: true,
-        type: "module",
-      },
-    }),
-  ],
+			devOptions: {
+				enabled: true,
+				navigateFallback: "index.html",
+				suppressWarnings: true,
+				type: "module",
+			},
+		}),
+	],
 });
